ab0b84de13633a941a5e42efa2d42bd37ece4af6
standardhub <199046031+standardhub@users.noreply.github.com>
2025-10-23 01:27:04 -0700

    feat:db

---
+++ db.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+func connectMongo() *mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	c, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatal("mongo connect error: %v", err)
+	}
+
+	// simple ping
+	if err := c.Ping(ctx, nil); err !=nil {
+		log.Fatal("mongo ping error: %v", err)
+	}
+
+	return c
+}
+
+func getUserCollection() *mongo.Collection {
+	return client.Database(mongoDatabase).Collection("users")
+}
