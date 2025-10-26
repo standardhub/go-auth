11bfdc14c3cde032768eefc9a88f11d22da02e07
standardhub <199046031+standardhub@users.noreply.github.com>
2025-10-26 10:44:50 -0700

    feat:register & login request

---
+++ handlers.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type registerRequest struct {
+	Email	 string `json:"email"`
+	password string `json:"password"`
+}
+
+type loginRequest struct {
+	Email 	 string `json:"email"`
+	password string `json:"password"`
+}
