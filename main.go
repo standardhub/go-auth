4ddbc8513e6914fdb98c66b0550fbdcef5b6199a
standardhub <199046031+standardhub@users.noreply.github.com>
2025-10-27 02:51:45 -0700

    fix

--- main.go
+++ main.go
@@ -3,24 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+)
 
-	func main() {
-		client = connectMongo()
-		defer func() {
-			if err := client.Disconnect(nil); err != nil {
-				log.Printf("error disconnecting mongo: %v", err)
-			}
+func main() {
+
+	client = connectMongo()
+	defer func() {
+		if err := client.Disconnect(nil); err != nil {
+			log.Printf("error disconnecting mongo: %v", err)
 		}
 	}()
-
+	
 	mux := http.NewServeMux()
 	mux.HandleFunc("/register", registerHandler)
 	mux.HandleFunc("/login", loginHandler)
-
+	
+	
 	mux.Handle("/profile", authMiddleware(http.HandlerFunc(profileHandler)))
-
+	
 	log.Println("server running on :8080")
-	if err := http.ListenAndServer(":8080", mux); err != nil {
+	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Fatalf("server error: %v", err)
 	}
-)
+}
